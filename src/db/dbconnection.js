f9bcd21ee7f9ed542fee3868cbb402e4d02bf2d4
subodh raturi BMDU <subodhfrontedbmdu@gmail.com>
2025-09-12 23:11:06 +0530

    util code

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
-    )
+    );
     console.log(
       ` mongoDB connected !! Database Host: ${connectionInstance.connection.host}`
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbconnection.js";
-
+import { app } from "./app.js";
 
 
 dotenv.config()
+
+// async always return a promis
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+            
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on :${process.env.PORT}`);
+        }) 
+        })
+    .catch((err) => {
+        console.log("mongodb connection fail !!!", err);
+    })
